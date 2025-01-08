9b34e40af25863f6a9c708a5102bacf0ce5c0aec
Tom <tom@marti.wf>
2025-01-08 16:01:46 +0100

    asdasd

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    (python3.withPackages (ps: with ps; with python3Packages; [
+        jupyter
+	pandas
+	scikit-learn
+	matplotlib
+	numpy	
+	seaborn
+	pytorch
+	shap
+    ]))
+  ];
+}
